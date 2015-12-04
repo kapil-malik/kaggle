997f4d62da04146cb2fa197d40047bbbc47b043f
kmalik <kmalik@adobe.com>
2015-12-04 17:53:57 +0530

    udf array to vector

--- sf-crimes/src/main/scala/com/kmalik/kaggle/sfcrimes/RfModeling.scala
+++ sf-crimes/src/main/scala/com/kmalik/kaggle/sfcrimes/RfModeling.scala
@@ -8,6 +8,7 @@ import org.apache.spark.sql.functions._
 import com.kmalik.kaggle.utils.Utils
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.ml.feature.StringIndexer
+import scala.collection.mutable.WrappedArray
 
 object RfModeling extends Serializable {
 
@@ -40,8 +41,6 @@ object RfModeling extends Serializable {
     
     val df = sqc.createDataFrame(labelData)
     			.toDF("label","row")
-    
-    saveDf(df, outputDir + "/df")
     			
     val dateIndex = DataProcessing.index(header, DataProcessing.DATES)
     val dowIndex = DataProcessing.index(header, DataProcessing.DAY_OF_WEEK)
@@ -61,12 +60,9 @@ object RfModeling extends Serializable {
     sqc.udf.register("weekend", 
         (row:String, dowIndex:Int) => DataProcessing.buildWeekend(row.split(",")(dowIndex)))
     
-//    sqc.udf.register("features", 
-//        (features:Array[Double]) => Vectors.dense(features))
-        
     sqc.udf.register("features", 
-        (f1:Double,f2:Double,f3:Double,f4:Double,f5:Double,f6:Double,f7:Double,f8:Double,f9:Double) 
-        	=> Vectors.dense(Array[Double](f1,f2,f3,f4,f5,f6,f7,f8,f9)))
+        (features:WrappedArray[Double]) => Vectors.dense(features.toArray))
+        
     
     val df2 = df.select(df("label"),
     				callUDF("strColumn", df("row"), lit(districtIndex)).as("district"),
@@ -79,22 +75,19 @@ object RfModeling extends Serializable {
             	 	callUDF("dateColumn", df("row"), lit(dateIndex), lit(3)).as("quarter"),
             	 	callUDF("weekend", df("row"), lit(dowIndex)).as("weekend")
             		)
-    saveDf(df2, outputDir + "/df2", ",")
-    
     val districtIndexer = new StringIndexer()
     							.setInputCol("district")
     							.setOutputCol("districtNum")
     val df3 = districtIndexer.fit(df2).transform(df2)
-    saveDf(df3, outputDir + "/df3", ",")
     
     val dowIndexer = new StringIndexer()
     							.setInputCol("dayOfWeek")
     							.setOutputCol("dayOfWeekNum")
     val df4 = dowIndexer.fit(df3).transform(df3)
-    saveDf(df4, outputDir + "/df4", ",")
     
     val df5 = df4.select(df4("label"), 
-    					callUDF("features", 
+    					callUDF("features",
+    							array(
     							df4("districtNum"),
 	    				    	df4("dayOfWeekNum"),
 	    				    	df4("xCoord"),
@@ -103,9 +96,9 @@ object RfModeling extends Serializable {
 	    				    	df4("month"),
 	    				    	df4("hr"),
 	    				    	df4("quarter"),
-	    				    	df4("weekend")
+	    				    	df4("weekend"))
 	    				        ).as("features"))
-    saveDf(df5, outputDir + "/df5", ",")
+    saveDf(df5, outputDir + "/df", ",")
     
     sc.stop()
   }
