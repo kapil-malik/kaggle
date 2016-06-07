0c7f6695bcd5d9638167bb4a442d2a858722c11b
kapil.malik <kapil.malik@snapdeal.com>
2016-06-08 01:46:21 +0530

    upgrading to spark 1.6.0

--- kaggle-utils/src/main/scala/com/kmalik/kaggle/utils/DfUtils.scala
+++ kaggle-utils/src/main/scala/com/kmalik/kaggle/utils/DfUtils.scala
@@ -2,16 +2,31 @@ package com.kmalik.kaggle.utils
 
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.SaveMode
 
 object DfUtils {
     
+  def load(sqc:SQLContext, location:String, format:String, 
+      options:Map[String,String] = Map()):DataFrame = {
+      sqc.read
+         .format(format)
+         .options(options)
+         .load(location)
+  }
+
+  def save(df:DataFrame, path:String, format:String, partitions:Int = 128,
+      options:Map[String,String] = Map()) = {
+      val baseDf:DataFrame = if(partitions > 0) df.coalesce(partitions) else df
+      baseDf.write
+            .mode(SaveMode.Overwrite)
+            .format(format)
+            .options(options)
+            .save(path)
+  }
+  
   def saveAsCsv(df:DataFrame, outPath:String, delim:String = ","):Unit = {
-	  df.coalesce(1)
-	    .write
-    	.format("com.databricks.spark.csv")
-    	.option("delimiter", delim)
-    	.option("header", "true")
-    	.save(outPath)
+    save(df, outPath, "com.databricks.spark.csv", 1, 
+        Map("delimiter"->delim, "header"->"true"))
   }
 
   def checkpoint(df:DataFrame, sqc:SQLContext, outPath:String, format:String = "parquet"):DataFrame = {
